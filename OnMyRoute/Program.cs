5c65fd0fcbd26c3dc2738386b86125eebc28a1e4
Leon van der Meer <64834803+leonvandermeer@users.noreply.github.com>
2024-02-17 21:36:36 +0100

    Log Application Startup and add Crash Handler

--- OnMyRoute/Program.cs
+++ OnMyRoute/Program.cs
@@ -1,4 +1,5 @@
-﻿using Octokit;
+﻿using Extensions.Logging.SingleRollingFile;
+using Octokit;
 using OnMyRoute;
 using OnMyRoute.Extensions.DependencyInjection;
 using System.Reflection;
@@ -6,9 +7,14 @@ using Updates.Updates;
 
 using Mutex appMutex = new(false, @"Global\On My Route AppMutex");
 
+Type type = typeof(Program);
+string product = type.Assembly.GetCustomAttribute<AssemblyProductAttribute>()!.Product;
+string currentVersion = type.Assembly.GetCustomAttribute<CurrentVersionAttribute>()!.Version;
+
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.AddWpf<App>();
 builder.Services
+    .AddCrashHandler(product, currentVersion)
     .AddTransient<MainWindow>()
     .AddTransient<MainViewModel>()
     .AddTransient<UpdatesViewModel>()
@@ -19,13 +25,13 @@ builder.Services
     .AddTransient(s => s.GetRequiredService<IGitHubClient>().Repository.Release)
     .AddHttpClient<UpdateProvider>().Services
     .Configure<UpdateData>(o => {
-        Type type = typeof(Program);
         o.Owner = "leonvandermeer";
         o.RepositoryName = "on-my-route";
         o.Company = type.Assembly.GetCustomAttribute<AssemblyCompanyAttribute>()!.Company;
-        o.Product = type.Assembly.GetCustomAttribute<AssemblyProductAttribute>()!.Product;
-        o.CurrentVersion = type.Assembly.GetCustomAttribute<CurrentVersionAttribute>()!.Version;
+        o.Product = product;
+        o.CurrentVersion = currentVersion;
         o.InstallUpdateWithin = TimeSpan.FromDays(1);
     });
+builder.Logging.AddRollingFile();
 IHost host = builder.Build();
 await host.RunAsync();
