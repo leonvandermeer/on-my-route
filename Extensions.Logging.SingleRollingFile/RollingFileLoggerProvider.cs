c658872c20fe50d694a4022d93955bb04109885c
Leon van der Meer <64834803+leonvandermeer@users.noreply.github.com>
2024-02-23 23:48:00 +0100

    Implement log scope

--- Extensions.Logging.SingleRollingFile/RollingFileLoggerProvider.cs
+++ Extensions.Logging.SingleRollingFile/RollingFileLoggerProvider.cs
@@ -4,14 +4,22 @@ using System.Collections.Concurrent;
 namespace Extensions.Logging.SingleRollingFile;
 
 [ProviderAlias("RollingFile")]
-internal class RollingFileLoggerProvider(RollingFileLoggerProcessor processor) : ILoggerProvider {
+internal class RollingFileLoggerProvider(RollingFileLoggerProcessor processor) : ILoggerProvider, ISupportExternalScope {
     private readonly ConcurrentDictionary<string, RollingFileLogger> loggers = new();
+    private IExternalScopeProvider? scopeProvider;
 
     public ILogger CreateLogger(string categoryName) {
         return loggers.TryGetValue(categoryName, out RollingFileLogger? logger) ?
             logger :
-            loggers.GetOrAdd(categoryName, new RollingFileLogger(categoryName, processor));
+            loggers.GetOrAdd(categoryName, new RollingFileLogger(categoryName, processor, scopeProvider));
     }
 
     public void Dispose() { }
+
+    public void SetScopeProvider(IExternalScopeProvider scopeProvider) {
+        this.scopeProvider = scopeProvider;
+        foreach (KeyValuePair<string, RollingFileLogger> logger in loggers) {
+            logger.Value.ScopeProvider = scopeProvider;
+        }
+    }
 }
